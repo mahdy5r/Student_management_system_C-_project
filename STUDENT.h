91aefc96a8c8ae339b5337c04fb321e0159223ca
Mahdy Ramadan <mahdyramadan2005@gmail.com>
2026-04-18 13:20:10 +0200

    Add files via upload

---
+++ STUDENT.h
@@ -0,0 +1,50 @@
+#ifndef STUDENT
+#define STUDENT
+
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <limits>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+struct student{
+    string Name;
+    uint32_t ID;
+    vector <double> grade_sub;
+};
+
+
+extern vector <student> students;
+extern const vector <string> name_sub;
+
+void print_Data_sub (const student& st);
+void Data_sub (student &st);
+void Update (student& st);
+void PrintStudent (const student& st);
+void Read_data();
+double Sum_grade (const student& st);
+void Write_data();
+string normalize(string s);
+bool ID_exists (const uint32_t id);
+
+//[1] Add New Student :
+void Addstudent ();
+//[2] Display All Students : 
+void Display();
+//[3] Search Student (by Name or ID) : 
+void Search();
+//[4] Delete Student :
+void Delete_student();
+// [5] Update student information :
+void Update_student();
+// [6] Show Top Students : 
+void Display_top_student();
+// [7] Sort Students by Grades :
+void Sort_all_student();
+
+#endif
